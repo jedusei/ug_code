22624dbe7285b405b9d4b1a371035f96aae96892
jedusei <jeduseiubs@gmail.com>
2018-10-15 18:08:28 +0100

    Update a5_10681279.cc

--- a5_10681279.cc
+++ a5_10681279.cc
@@ -34,7 +34,7 @@ int main()
 
 void ReceiveInput()
 {
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < 5; i++)
     {
         Student s;
 
@@ -42,7 +42,7 @@ void ReceiveInput()
         cout << "ID: ";
         cin >> s.id;
         cout << "Name: ";
-        getline(cin, s.name);
+        cin >> s.name;
         cout << "Age: ";
         cin >> s.age;
         cout << "Gender: ";
@@ -54,7 +54,18 @@ void ReceiveInput()
         cout << endl;
 
         students[i] = s;
+
+        avgAge += s.age;
+        avgScore += s.score;
+
+        if (s.gender == 'M' || s.gender == 'm')
+            maleCount++;
+        else
+            femaleCount++;
     }
+
+    avgAge /= 5.0;
+    avgScore /= 5.0;
 }
 
 void StoreData()
@@ -67,10 +78,18 @@ void StoreData()
     for (int i = 0; i < 5; i++)
     {
         Student s = students[i];
-        file << setfill('0') << setw(8) << s.id << "";
-        file << s.name << endl;
+        file << setfill('0') << setw(8) << s.id << "  ";
+        file << s.name << "  " << s.age << " ";
+        file << s.gender << "  " << s.score << " ";
+        file << s.grade << endl;
     }
 
+    file << endl;
+    file << "Average Age: " << avgAge << endl;
+    file << "Average Score: " << avgScore << endl;
+    file << "Male Count: " << maleCount << endl;
+    file << "Female Count: " << femaleCount << endl;
     file.close();
+
     cout << "Data saved in: " << fileName << endl;
 }
