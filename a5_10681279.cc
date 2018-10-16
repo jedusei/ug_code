74b8294d9f3915acdabbac1c20596656da10ebcd
jedusei <jeduseiubs@gmail.com>
2018-10-16 06:51:28 +0100

    Update a5_10681279.cc

--- a5_10681279.cc
+++ a5_10681279.cc
@@ -32,9 +32,31 @@ int main()
     return 0;
 }
 
+string GetGrade(int score)
+{
+    if (score >= 80)
+        return "A";
+    else if (score >= 75)
+        return "B+";
+    else if (score >= 70)
+        return "B";
+    else if (score >= 65)
+        return "C+";
+    else if (score >= 60)
+        return "C";
+    else if (score >= 55)
+        return "D+";
+    else if (score >= 50)
+        return "D";
+    else if (score >= 45)
+        return "E";
+    else
+        return "F";
+}
+
 void ReceiveInput()
 {
-    cout << "Please enter the student details below:" << endl
+    cout << "Please enter the student details below:\n"
          << endl;
     for (int i = 0; i < 5; i++)
     {
@@ -44,17 +66,16 @@ void ReceiveInput()
         cout << "ID: ";
         cin >> s.id;
         cout << "Name: ";
-        cin >> s.name;
+        getline(cin, s.name);
         cout << "Age: ";
         cin >> s.age;
         cout << "Gender (M/F): ";
         cin >> s.gender;
         cout << "Score: ";
         cin >> s.score;
-        cout << "Grade: ";
-        cin >> s.grade;
         cout << endl;
 
+        s.grade = GetGrade(s.score);
         students[i] = s;
 
         avgAge += s.age;
@@ -62,7 +83,7 @@ void ReceiveInput()
 
         if (s.gender == 'M' || s.gender == 'm')
             maleCount++;
-        else
+        else if (s.gender == 'F' || s.gender == 'f')
             femaleCount++;
     }
 
@@ -74,8 +95,8 @@ void StoreData()
 {
     ofstream file;
     file.open(fileName, fstream::out);
-    file << "ID\t\tName\t\tAge\t\tGender\t\tScore\t\tGrade" << endl;
-    file << "*********************************************" << endl;
+    file << "ID\t\t\tName\t\tAge\t\tGender\t\tScore\t\tGrade" << endl;
+    file << "*************************************************" << endl;
 
     for (int i = 0; i < 5; i++)
     {
@@ -86,7 +107,8 @@ void StoreData()
         file << s.grade << endl;
     }
 
-    file << "*********************************************" << endl << endl;
+    file << "*************************************************" << endl
+         << endl;
     file << "Average Age: " << avgAge << endl;
     file << "Average Score: " << avgScore << endl;
     file << "Male Count: " << maleCount << endl;
